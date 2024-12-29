194c1c8404ef6c4471b1941537434fa56e055751
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2024-12-29 12:35:28 +0100

    add page transition with staggered animation

--- components/PageTransition.tsx
+++ components/PageTransition.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { AnimatePresence, motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 
@@ -7,11 +8,14 @@ const PageTransition: React.FC<{ children: React.ReactNode }> = ({ children }) =
     return (
         <AnimatePresence>
             <div key={pathName}>
-                <motion.div initial={{ opacity: 1 }}
+                <motion.div
+                    initial={{ opacity: 1 }}
                     animate={{
                         opacity: 0,
                         transition: {
-                            delay: 1, duration: 0.4, ease: "easeInOut"
+                            delay: 1,
+                            duration: 0.4,
+                            ease: "easeInOut"
                         },
                     }}
                     className="h-screen w-screen fixed bg-primary top-0 pointer-events-none"
@@ -20,6 +24,6 @@ const PageTransition: React.FC<{ children: React.ReactNode }> = ({ children }) =
             </div>
         </AnimatePresence>
     );
-}
+};
 
 export default PageTransition;
