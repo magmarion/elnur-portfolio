5cf5f0007b128d807f167643ac5466f149e9229b
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2024-12-31 23:03:52 +0100

    add toggleMenu logic into the mobile nav sheet

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -2,7 +2,9 @@
 
 // Components to build a mobile navigation menu
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 import Link from "next/link";
 // installed hamburger menu icon from react-icons using "npm i react-icons"
 import { CiMenuFries } from "react-icons/ci";
@@ -36,26 +38,40 @@ const links = [
 
 const MobileNav = () => {
     const pathname = usePathname();
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-        <Sheet>
-            <SheetTrigger className="flex justify-center items-center">
+        // onOpenChange is used to open and close the menu using X button and clicking anywhere outside of the Sheet
+        <Sheet open={isOpen} onOpenChange={setIsOpen}>
+            <SheetTrigger onClick={toggleMenu} className="flex justify-center items-center">
                 <CiMenuFries className="text-[32px] text-accent" />
             </SheetTrigger>
             <SheetContent className="flex flex-col">
 
-                <div className="mt-32 mb-40 text-center text-2xl">logo
-                    <Link href="/">
+                <div className="mt-32 mb-40 text-center text-2xl">
+                    {/* by using preventDefault(), the menu will be closed when the logo is clicked, 
+                    but the link will not be followed. */}
+                    <Link href="/" onClick={(e) => {
+                        e.preventDefault();
+                        toggleMenu()
+                    }}>
                         <h1 className="text-4xl font-semibold">
                             Elnur<span className="text-accent">.</span>
                         </h1>
                     </Link>
                 </div>
+
                 {/* nav */}
                 <nav className="flex flex-col justify-center items-center gap-8">
                     {links.map((link, index) => {
                         return (
                             <Link href={link.path}
                                 key={index}
+                                onClick={toggleMenu}
                                 className={`${link.path === pathname && "text-accent border-b-2 border-accent"
                                     } text-xl capitalize hover:text-accent transition-all`}
                             >
