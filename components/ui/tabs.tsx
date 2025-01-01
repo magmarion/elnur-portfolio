878ed1bac520f896c8caae61f49dc2bafb3ea0b9
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2025-01-01 15:05:51 +0100

    modify styles of tabs in tabs ui component

--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -28,8 +28,12 @@ const TabsTrigger = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <TabsPrimitive.Trigger
     ref={ref}
-    className={cn(
-      "inliine-flex w-full items-center justify-center bg-[#27272c]  whitespace-nowrap text-white rounded-xl p-3 text-balance font-medium ring-offset-white transition-all disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-accent data-[state=active]:text-primary data-[state=active]:font-bold data-[state=active]:shadow-sm",
+    className={cn(`
+      inliine-flex w-full items-center justify-center bg-[#27272c] whitespace-nowrap
+      text-white rounded-xl p-3 text-base font-medium ring-offset-white transition-all
+      disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-accent
+      data-[state=active]:text-primary data-[state=active]:font-bold data-[state=active]:shadow-sm
+      `,
       className
     )}
     {...props}
@@ -43,8 +47,11 @@ const TabsContent = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <TabsPrimitive.Content
     ref={ref}
-    className={cn(
-      "mt-2 ring-offset-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300",
+    className={cn(`
+      min-h-[480px] ring-offset-white focus-visible:outline-none focus-visible:ring-2
+      focus-visible:ring-slate-950 focus-visible:ring-offset-2 dark:ring-offset-slate-950
+      dark:focus-visible:ring-slate-300
+      `,
       className
     )}
     {...props}
