df04a9e95f547f812fcc1f7ab7f1962625458e54
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2025-01-01 12:51:05 +0100

    add tabs and style

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -165,18 +165,29 @@ const Resume = () => {
             initial={{ opacity: 0 }}
             animate={{
                 opacity: 1,
-                transition: { duration: 0.4, ease: "easeInOut" },
+                transition: { delay: 1, duration: 0.4, ease: "easeIn" },
             }}
             className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
         >
             <div className="container mx-auto">
-                <Tabs>
-                    <TabsList>
+                <Tabs
+                    defaultValue="experience"
+                    className="flex flex-col xl:flex-row gap-16"
+                >
+                    <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
                         <TabsTrigger value="experience">Experience</TabsTrigger>
                         <TabsTrigger value="education">Education</TabsTrigger>
                         <TabsTrigger value="skills">Skills</TabsTrigger>
                         <TabsTrigger value="about">About me</TabsTrigger>
                     </TabsList>
+                    {/* content */}
+                    <div className="min-h-[70vh] w-full">
+                        {/* experience */}
+                        <TabsContent value="experience" className="w-full">
+                            experience
+                        </TabsContent>
+
+                    </div>
                 </Tabs>
             </div>
         </motion.div>
