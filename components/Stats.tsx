da44f49584ce6d8e2bf1f873eef1231962d2087d
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2025-01-15 18:23:31 +0100

    fix delay on stats count up

--- components/Stats.tsx
+++ components/Stats.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import CountUp from "react-countup";
+import { motion } from "framer-motion";
 
 const stats = [
     {
@@ -25,32 +26,42 @@ const Stats = () => {
     return (
         <section className="pt-4 pb-12 xl:pt-0 xl:pb-12">
             <div className="container mx-auto">
-                <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
+                <motion.div
+                    initial={{ opacity: 0 }} // Startar osynligt
+                    animate={{ opacity: 1 }} // Gör det synligt
+                    transition={{
+                        delay: 2.4,
+                        duration: 0.5,
+                        ease: "easeIn"
+                    }}
+                    className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none"
+                >
+
                     {stats.map((item, index) => {
                         return (
                             <div
                                 className="flex-1 flex gap-4 items-center justify-center
-                            xl:justify-start"
+                                xl:justify-start"
                                 key={index}
                             >
 
                                 <CountUp
                                     end={item.num}
                                     duration={5}
-                                    delay={2}
+                                    delay={3}
                                     className="text-4xl xl:text-6xl font-extrabold text-accent"
                                 />
-                                <p
-                                    className={`${item.text.length > 15 ? "max-w-[100px]" : "max-w-[150px]"
-                                        } leading-snug text-white/80`}
+                                <p className={`${item.text.length > 15 ? "max-w-[100px]" : "max-w-[150px]"
+                                    } leading-snug text-white/80`}
                                 >
-                                    {item.text}</p>
+                                    {item.text}
+                                </p>
                             </div>
-                        )
+                        );
                     })}
-                </div>
+                </motion.div>
             </div>
-        </section>
+        </section >
     );
 };
 
