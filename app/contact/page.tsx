dab4bc6dd6efe94cb86cfeb623c82feb3b4a2caf
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2025-01-18 17:24:43 +0100

    remove phone from contact page and add new bg-color for the form box

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,23 +5,18 @@ import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 
 import { Select, SelectContent, SelectItem, SelectGroup, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+import { FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
 
 const info = [
     {
-        icon: <FaPhoneAlt />,
-        title: "Phone",
-        description: "(+46) xxxxxxxxx",
-    },
-    {
         icon: <FaEnvelope />,
         title: "Email",
-        description: "myemail@gmail.com",
+        description: "Elnur.Guliyev@medieinstitutet.se",
     },
     {
         icon: <FaMapMarkerAlt />,
         title: "Address",
-        description: "Kod Torget 9, Tech City 123 45",
+        description: "Metropolitan Gothenburg",
     },
 ];
 
@@ -40,7 +35,7 @@ const Contact = () => {
             <div className="flex flex-col xl:flex-row gap-[30px]">
                 {/* form */}
                 <div className="xl:w-[54%] order-2 xl:order-none">
-                    <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+                    <form className="flex flex-col gap-6 p-10 bg-[#171212] rounded-xl">
                         <h3 className="text-4xl text-accent">Let's work together</h3>
                         <p className="text-white/60">
                             I'm a passionate front-end developer with experience in creating visually appealing,
@@ -88,7 +83,7 @@ const Contact = () => {
                             return (
                                 <li key={index} className="flex items-center gap-6">
                                     <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px]
-                                    bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                                    bg-[#171212] text-accent rounded-md flex items-center justify-center">
                                         <div className="text-[28px]">{item.icon}</div>
                                     </div>
                                     <div className="flex-1">
